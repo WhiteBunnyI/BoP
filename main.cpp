d526fa21a741878a3f96952d2b9819bde5e2f74c
WhiteBunnyI <144366124+WhiteBunnyI@users.noreply.github.com>
2023-09-22 16:24:53 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+
+int main()
+{
+    long long mul = 1;
+    int greatest = 0;
+    int index = 0;
+
+    for (int A = 0; A < 20; A++) 
+    {
+        if (A % 5) 
+        {
+            mul *= A;
+
+            if (greatest < A)
+            {
+                greatest = A;
+                index = A;
+            }
+
+        }
+    }
+
+    std::cout << "Multiply: " << mul << " Greatest number: " << greatest << " Index of greatest number: " << index;
+
+    return 0;
+}
+
+

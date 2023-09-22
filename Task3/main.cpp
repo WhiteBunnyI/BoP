83bd61a2e63c3a4bfe3e9fd06fe6e13c4015290d
WhiteBunnyI <144366124+WhiteBunnyI@users.noreply.github.com>
2023-09-22 16:13:37 +0200

    Add files via upload

---
+++ Task3/main.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+int main()
+{
+    int x;
+    std::cin >> x;
+
+	if (x == 1) 
+	{
+		std::cout << "Positive number \n";
+	}
+	else if (x == -1) 
+	{
+		std::cout << "Negative number \n";
+	}
+	else 
+	{
+		std::cout << "Unknown number \n";
+	}
+
+    return 0;
+}
+
+

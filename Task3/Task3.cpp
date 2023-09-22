6df676f077fb3e648d9a80e8033c6caaee418c9d
WhiteBunnyI <144366124+WhiteBunnyI@users.noreply.github.com>
2023-09-22 15:57:21 +0200

    Update and rename Task3.cpp to Task3/Task3.cpp

--- Task3/Task3.cpp
+++ Task3/Task3.cpp
@@ -6,7 +6,7 @@ int Task3()
 
     int a, b, c;
 
-    std::cout << "Ââåäèòå ÷èñëà A, B, C: \n";
+    std::cout << "Ã‚Ã¢Ã¥Ã¤Ã¨Ã²Ã¥ Ã·Ã¨Ã±Ã«Ã  A, B, C: \n";
     std::cin >> a >> b >> c;
 
     if (!(a % c) && !(b % c)) std::cout << ((a + b) / c);

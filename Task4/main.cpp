7628d51ebee984a2a12e413057a98d490fd6b228
WhiteBunnyI <144366124+WhiteBunnyI@users.noreply.github.com>
2023-09-22 16:55:59 +0200

    Add files via upload

---
+++ Task4/main.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+int main()
+{
+    int n;
+    int mul = 1;
+    std::cin >> n;
+
+    while (n)
+    {
+        int rest = n % 10;
+        n = n / 10;
+        if (rest % 2)
+        {
+            mul *= rest;
+        }
+    }
+    std::cout << mul;
+    return 0;
+}
+
+

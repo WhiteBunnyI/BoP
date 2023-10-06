06723a491066fec2a38977f419044b136c3371f7
WhiteBunnyI <144366124+WhiteBunnyI@users.noreply.github.com>
2023-10-06 22:11:23 +0200

    Add files via upload

---
+++ Task5/main.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+
+
+int main()
+{
+    const int N = 5;
+    int mas[N];
+
+    for (int i = 0; i < N; i++) std::cin >> mas[i];
+
+    for (int i = 0; i < N; i++)
+    {
+        int temp = mas[i];
+        mas[i] = 0;
+        mas[i] = temp;
+    }
+
+    for (int i = 0; i < N; i++)
+    {
+        bool found = false;
+        if (i == 0)
+        {
+            std::cout << mas[i] << " ";
+            continue;
+        }
+        for (int o = 0; o < i; o++)
+        {
+            if (mas[i] == mas[o])
+            {
+                found = true;
+                break;
+            }
+        }
+        if(!found) std::cout << mas[i] << " ";
+        
+    }
+    return 0;
+}
+
+
